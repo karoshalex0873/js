a9910a55c26dfc9fce5a772a032a4cad7cdcd785
Alex Karobia <akarobia30@gmail.com>
2025-01-24 12:33:41 +0300

    Exercise one

---
+++ js_questions/question1.js
@@ -0,0 +1,16 @@
+// Extercise 1: Write a JavaScript program to check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100
+
+let Num1 =80;
+let Num2 = 10;
+if (Num1 ===100){
+    console.log("first number is 100");
+}
+else if(Num2 === 100){
+    console.log("second number is 100");
+}
+else if(Num1 + Num2 === 100){
+    console.log("Sum of Both numbers is 100");
+}
+else{
+    console.log("None of them are 100");
+}
