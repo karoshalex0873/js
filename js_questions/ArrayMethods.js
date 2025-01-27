7a8daa21ae005c873d6ae180a0f6d10e944c53bf
Alex Karobia <akarobia30@gmail.com>
2025-01-27 19:20:03 +0300

    Introduction to Arrays in js

---
+++ js_questions/ArrayMethods.js
@@ -0,0 +1,8 @@
+// arryays in javascript
+
+const Fruits = []
+Fruits.push('Apple','banana','mango','orange')
+Fruits[4]="pineapple"
+console.log(`${Fruits[4]} is added to the array of fruits`)
+console.log(`The Number of fruits  in the store are ${Fruits.length}`)
+console.log(`Here is the list of the fruits in the store:  ${Fruits}`)
